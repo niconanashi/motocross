fa4466743d645b7c69ef350bb806a20d2d1ffded
niconanashi <79348397+niconanashi@users.noreply.github.com>
2024-04-09 17:57:35 +0900

    Delete script directory

--- script/mainScene.js
+++
@@ -1,9 +0,0 @@
-window.gLocalAssetContainer["mainScene"] = function(g) { (function(exports, require, module, __filename, __dirname) {
-"use strict";
-var mainSceneController_1 = require("./common/mainSceneController");
-module.exports = function () {
-    return mainSceneController_1.MainSceneController.createMainScene(g.game);
-};
-
-})(g.module.exports, g.module.require, g.module, g.filename, g.dirname);
-}
