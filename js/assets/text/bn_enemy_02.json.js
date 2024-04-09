e05af67dc117a13a5b56281e5e7688e56f553ec8
niconanashi <79348397+niconanashi@users.noreply.github.com>
2024-04-09 17:58:13 +0900

    Delete js/assets/text directory

--- js/assets/text/bn_enemy_02.json.js
+++
@@ -1,1 +0,0 @@
-window.gLocalAssetContainer["bn_enemy_02"] = "{%22version%22:%222.0.0%22,%22contents%22:{%22name%22:%22enemy_02%22,%22bones%22:[{%22arrayIndex%22:0,%22parentIndex%22:-1,%22name%22:%22root%22,%22colliderInfos%22:[]},{%22arrayIndex%22:1,%22parentIndex%22:0,%22name%22:%22obstacle_rock_01%22,%22colliderInfos%22:[]}]}}"
--- js/assets/text/bn_enemy_03.json.js
+++
@@ -1,1 +0,0 @@
-window.gLocalAssetContainer["bn_enemy_03"] = "{%22version%22:%222.0.0%22,%22contents%22:{%22name%22:%22enemy_03%22,%22bones%22:[{%22arrayIndex%22:0,%22parentIndex%22:-1,%22name%22:%22root%22,%22colliderInfos%22:[]},{%22arrayIndex%22:1,%22parentIndex%22:0,%22name%22:%22obstacle_shark_01%22,%22colliderInfos%22:[]}]}}"
