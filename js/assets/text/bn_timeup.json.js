648e992800e2bfd371d4b8eacd3272f30fe2a264
niconanashi <79348397+niconanashi@users.noreply.github.com>
2024-04-09 18:04:16 +0900

    Add files via upload

---
+++ js/assets/text/bn_timeup.json.js
@@ -0,0 +1,1 @@
+window.gLocalAssetContainer["bn_timeup"] = "{%22version%22:%222.0.0%22,%22contents%22:{%22name%22:%22timeup%22,%22bones%22:[{%22arrayIndex%22:0,%22parentIndex%22:-1,%22name%22:%22root%22,%22colliderInfos%22:[]},{%22arrayIndex%22:1,%22parentIndex%22:0,%22name%22:%22m_timeup%22,%22colliderInfos%22:[]}]}}"
